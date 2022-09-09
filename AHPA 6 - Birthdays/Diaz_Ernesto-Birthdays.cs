cfc20637ed704470c099c0d9c0c9a05becb73648
Ernesto Diaz <84555026+zaidotsenre@users.noreply.github.com>
2022-09-08 20:24:32 -0400

    Initial commit for AHPA 6 - Birthdays. Does what it needs to do. May need to add formatting, input validation, and error handling for extra niceness.

---
+++ AHPA 6 - Birthdays/Diaz_Ernesto-Birthdays.cs
@@ -0,0 +1,33 @@
+﻿// COP 4931 - IT Software Systems Development
+// Instructor - James Anderson
+// Student - Ernesto Diaz
+// Assignment - AHPA 6: Birthdays
+
+// Create array with names and print to console
+string[] students = new string[] { "Jen Thompson", "Mike Hatch", "Ryan Jenkins" };
+Console.WriteLine("Students:");
+foreach(string student in students)
+{
+    Console.WriteLine(student);
+}
+
+Console.WriteLine();
+
+// Capitalize names and print to console with happy birthday messages
+foreach(string student in students)
+{
+    Console.WriteLine($"Happy birthday, {student.ToUpper()}!");
+}
+
+Console.WriteLine();
+
+// Ask for date of birth of each student
+// Calculate and output days alive
+foreach(string student in students)
+{
+    Console.WriteLine($"In what year was {student} born?");
+    
+    int birthYear = int.Parse(Console.ReadLine());
+    int daysAlive = (DateTime.Today.Year - birthYear) * 365;
+    Console.WriteLine($"{student} has been alive for approximately {daysAlive} days.");
+}
