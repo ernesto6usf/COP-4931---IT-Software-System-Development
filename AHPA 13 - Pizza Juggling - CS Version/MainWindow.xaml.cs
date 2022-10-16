06e474787c0b6c05d1c75ecc4de0cb73fc352259
Ernesto Diaz <84555026+zaidotsenre@users.noreply.github.com>
2022-10-16 16:59:13 -0400

    Initial commit for AHPA 13

---
+++ AHPA 13 - Pizza Juggling - CS Version/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AHPA_13___Pizza_Juggling___CS_Version
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Label label = new Label();
+            label.FontSize = 25;
+            label.Content = "Welcome to the 1st Annual Pizza Juggling Competition";
+            this.gridLayout.Children.Add(label);
+        }
+    }
+}
